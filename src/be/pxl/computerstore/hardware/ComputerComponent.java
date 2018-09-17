c35697e6f34b28224931b70ab9999d64410b2bed
Mariel Latella Barros <mariel.latella@gmail.com>
2018-09-17 15:31:50 +0200

    Add method to automatically generate an article number using a part of the vendor name and a random number. Restrict the possibility of changing the vendor by deleting setVendor().

--- src/be/pxl/computerstore/hardware/ComputerComponent.java
+++ src/be/pxl/computerstore/hardware/ComputerComponent.java
@@ -1,23 +1,24 @@
 package be.pxl.computerstore.hardware;
 
+import java.util.Random;
+
 public class ComputerComponent {
     private String vendor;
     private String name;
     private double price;
+    private String articleNumber;
 
     public ComputerComponent(String vendor, String name, double price) {
         this.vendor = vendor;
         this.name = name;
         this.price = price;
+        this.articleNumber = createArticleNumber(vendor);
     }
 
     public String getVendor() {
         return vendor;
     }
 
-    public void setVendor(String vendor) {
-        this.vendor = vendor;
-    }
 
     public String getName() {
         return name;
@@ -33,5 +34,32 @@ public class ComputerComponent {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public String createArticleNumber(String vendor) {
+        this.articleNumber = vendor.substring(0, 3).toUpperCase();
+
+
+        while (this.articleNumber.length() < 3) {
+            this.articleNumber += "X";
+        }
+
+        this.articleNumber += "-" + createRandomNumber();
+
+
+        return this.articleNumber;
+    }
+
+    private String createRandomNumber() {
+        Random rand = new Random();
+        int articleRand = rand.nextInt(100000);
+        String finalRandom = Integer.toString(articleRand);
+
+        while (finalRandom.length() < 4) {
+
+            finalRandom = "0" + finalRandom;
+        }
+
+        return finalRandom;
     }
 }
